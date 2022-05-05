d0fe2ab62b2439eef54ea04343af01da2e0507ee
fwiedmann <privat@felixwiedmann.de>
2022-05-05 22:38:20 +0200

    feat(social): add social links

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavigationComponent} from './components/navigation/navigation.component';
 
-import { WorkComponent } from './components/work/work.component';
-import { SkillsComponent } from './components/skills/skills.component';
-import { OpinionsComponent } from './components/opinions/opinions.component';
-import { InfoCardTextComponent } from './components/info-card-text/info-card-text.component';
-import { InfoCardPictureComponent } from './components/info-card-picture/info-card-picture.component';
+import {WorkComponent} from './components/work/work.component';
+import {SkillsComponent} from './components/skills/skills.component';
+import {OpinionsComponent} from './components/opinions/opinions.component';
+import {InfoCardTextComponent} from './components/info-card-text/info-card-text.component';
+import {InfoCardPictureComponent} from './components/info-card-picture/info-card-picture.component';
+import {TablerIconsModule} from "angular-tabler-icons";
+import {IconBrandGithub, IconBrandLinkedin, IconMenu} from "angular-tabler-icons/icons";
+import {SocialLinksComponent} from './components/social-links/social-links.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+
+
+const icons = {
+  IconBrandLinkedin,
+  IconBrandGithub,
+  IconMenu
+};
 
 @NgModule({
   declarations: [
@@ -20,12 +33,18 @@ import { InfoCardPictureComponent } from './components/info-card-picture/info-ca
     OpinionsComponent,
     InfoCardTextComponent,
     InfoCardPictureComponent,
+    SocialLinksComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TablerIconsModule.pick(icons),
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
